d393c3d56ab90e0bc7ef69ede8b4390aed278b84
Estefania <estgantri@alum.us.es>
2021-03-30 11:53:53 +0200

    Validations
    A validator has been created for the booking form and the exceptions have been created. #28

--- src/main/java/org/springframework/samples/petclinic/web/BookingController.java
+++ src/main/java/org/springframework/samples/petclinic/web/BookingController.java
@@ -9,9 +9,9 @@ import org.springframework.samples.petclinic.model.Booking;
 import org.springframework.samples.petclinic.model.Pet;
 import org.springframework.samples.petclinic.service.BookingService;
 import org.springframework.samples.petclinic.service.PetService;
+import org.springframework.samples.petclinic.service.exceptions.DateOverlapException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
@@ -36,6 +36,11 @@ public class BookingController {
 		dataBinder.setDisallowedFields("id");
 	}
 	
+	@InitBinder("booking")
+	public void initPetBinder(WebDataBinder dataBinder) {
+		dataBinder.setValidator(new BookingValidator());
+	}
+	
 	@ModelAttribute("booking")
 	public Booking loadBookingWithPet(@PathVariable("petId") final int petId) {
 		final Pet pet = this.petService.findPetById(petId);
@@ -51,16 +56,17 @@ public class BookingController {
 
 	@PostMapping(value = "/owners/{ownerId}/pets/{petId}/booking/new")
 	public String processNewBookingForm(@Valid final Booking booking, final BindingResult result) {
-		if(booking.getFinishDate()!=null && booking.getStartDate()!=null && !booking.getFinishDate().isAfter(booking.getStartDate())) {
-			final FieldError error = new FieldError("finishDate","finishDate", "Finish date must be after start date");
-			result.addError(error);
-		}
 		if (result.hasErrors()) {
 			return "pets/createOrUpdateBookingForm";
 		}
 		else {
-			this.bookingService.saveBooking(booking);
-			return "redirect:/owners/{ownerId}";
+			 try{
+				this.bookingService.saveBooking(booking);
+             }catch(DateOverlapException ex){
+                 result.rejectValue("startDate", "DateOverlap", "There is already a booking on that date");
+                 return "pets/createOrUpdateBookingForm";
+             }
+				return "redirect:/owners/{ownerId}";
 		}
 	}
 	
